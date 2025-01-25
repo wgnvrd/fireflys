fd593bcccf0f5edd48621ce8c4adff2988134687
RanivG <ranivgupta@gmail.com>
2025-01-25 17:59:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ import pandas as pd
 
 url = 'https://firms.modaps.eosdis.nasa.gov/mapserver/mapkey_status/?MAP_KEY=' + MAP_KEY + '/MODIS_NRT/-125,25,-64,50' #url and filter bounds for contigous US
 
-det_area = pd.read_csv(url) # data from url
+dat = pd.read_csv(url) # data from url
+
+det = dat[dat["confidence"] >= 50.0] # detected fires of concern
+
+det.describe() # debug and verify confidence is above 50
+
+det.sort_values(by = 'confidence')
+
+
+
+
+
+
 
 
 
