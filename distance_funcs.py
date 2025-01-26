b96527bdf328b5badce311a45b040cd6ac658667
Konoha Tomono-Duval <konohatomonoduval@gmail.com>
2025-01-26 04:52:14 -0600

    changed flightaware_test to distance_funcs

--- distance_funcs.py
+++ distance_funcs.py
@@ -5,36 +5,40 @@ load_dotenv()
 access_key = os.getenv('FLIGHTAWARE_KEY')
  
 api_url = "https://aeroapi.flightaware.com/aeroapi"
-params = {"query":'-latlong "50 -130 25 -60"'}
+us_bounding = {"query":'-latlong "50 -130 25 -60"'}
 
 AeroAPI = requests.Session()
 AeroAPI.headers.update({"x-apikey": access_key})
 
-result = AeroAPI.get(f"{api_url}/flights/search", params=params)
+result = AeroAPI.get(f"{api_url}/flights/search", params=us_bounding)
 us_flights = result.json()
 
-def find_closest(fire_lat, fire_long):
+def find_closest_flight(fire_lat, fire_long):
     #pythagorian theorum for flight with closest lat/long
     closest_flight = min(us_flights["flights"], key = lambda x: math.sqrt(abs(fire_lat - x["last_position"]["latitude"])^2 + abs(fire_long - x["last_position"]["longitude"])^2))
     return closest_flight["last_position"]["latitude"], closest_flight["last_position"]["longitude"]
-"""
-def next_x_waypoints(x_points, flight):
+
+def next_x_waypoints(x_points, flight, get_all = False):
+    packaged_waypoints = [flight["waypoints"][x:x+2] for x in range(0, len(flight["waypoints"]), 2)]
+
     if len(flight["waypoints"]) == 0:
         print("No waypoints available for flight " + flight["ident"])
+    elif get_all:
+        return packaged_waypoints
     else: 
         current_position = flight["last_position"]["latitude"], flight["last_position"]["longitude"]
-        for i in range(0, len(flight["waypoints"]), 2):
-        closest_waypoint_index = min(flight["waypoints"], key = lambda x: )
-            
+        closest_waypoint = min(flight["waypoints"], key = lambda x: math.sqrt(abs(packaged_waypoints[x][0] - flight["last_position"]["latitude"])^2 + abs(packaged_waypoints[x][1] - flight["last_position"]["longitude"])^2))
+        print("current position: " + str(current_position))
+        return closest_waypoint
 
 def find_closest_route(fire_lat, fire_long):
     flight_with_closest_waypoint = 2
-"""
+
 
 if __name__ == "__main__": 
     with open("us_flights.json", 'w', encoding='utf-8') as f:
         json.dump(us_flights, f, ensure_ascii=False, indent=4) 
-    #print(json.dumps(find_closest(35, -90), indent=4))
-    #print(find_closest(35, -90))
+    #print(json.dumps(find_closest_flight(35, -90), indent=4))
+    #print(find_closest_flight(35, -90))
     example_flight = us_flights["flights"][2]
-    #next_x_waypoints(10, example_flight)
+    next_x_waypoints(10, example_flight)
