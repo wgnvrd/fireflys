19b122569e4a060ad7327083945e66c6cdd5a294
Konoha Tomono-Duval <konohatomonoduval@gmail.com>
2025-01-26 02:17:28 -0600

    as i fell asleep

--- flightaware_test.py
+++ flightaware_test.py
@@ -17,19 +17,19 @@ def find_closest(fire_lat, fire_long):
     #pythagorian theorum for flight with closest lat/long
     closest_flight = min(us_flights["flights"], key = lambda x: math.sqrt(abs(fire_lat - x["last_position"]["latitude"])^2 + abs(fire_long - x["last_position"]["longitude"])^2))
     return closest_flight["last_position"]["latitude"], closest_flight["last_position"]["longitude"]
-
+"""
 def next_x_waypoints(x_points, flight):
     if len(flight["waypoints"]) == 0:
         print("No waypoints available for flight " + flight["ident"])
     else: 
         current_position = flight["last_position"]["latitude"], flight["last_position"]["longitude"]
         for i in range(0, len(flight["waypoints"]), 2):
-                
+        closest_waypoint_index = min(flight["waypoints"], key = lambda x: )
             
 
 def find_closest_route(fire_lat, fire_long):
     flight_with_closest_waypoint = 2
-
+"""
 
 if __name__ == "__main__": 
     with open("us_flights.json", 'w', encoding='utf-8') as f:
@@ -37,4 +37,4 @@ if __name__ == "__main__":
     #print(json.dumps(find_closest(35, -90), indent=4))
     #print(find_closest(35, -90))
     example_flight = us_flights["flights"][2]
-    next_x_waypoints(10, example_flight)
+    #next_x_waypoints(10, example_flight)
