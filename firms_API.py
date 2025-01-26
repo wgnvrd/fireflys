6acb6c3334c51e68c88d59d5acb844042c32c24a
RanivG <ranivgupta@gmail.com>
2025-01-26 05:58:33 -0600

    main to firms_API function

---
+++ firms_API.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import distance_funcs as dist
+
+load_dotenv()
+
+def firms_API(W = -125.0 , S = 25.0, E = -64.0, N = 50.0, confLwrBnd = 50.0, confUpperBnd = 100.0):
+    MAP_KEY = os.getenv('FIRMS_MAP_KEY')
+
+
+    url = 'https://firms.modaps.eosdis.nasa.gov/api/area/csv/' + str(MAP_KEY) + '/MODIS_NRT/'+ str(W) + str(S) + str(E) + str(N) +'/10' #url and filter bounds for contigous US
+
+
+    dat = pd.read_csv(url) # data from url
+
+    #confLwrBnd = 50.0
+    #confUpperBnd = 100.0
+
+    det = dat.sort_values(by='confidence' , ascending=False)
+
+    det = det[det["confidence"] >= confLwrBnd] # detected fires of concern
+    det = det[det['confidence'] <= confUpperBnd] #filter out confirmed fires
+
+    lat = det["latitude"].to_numpy()
+    lon = det["longitude"].to_numpy()
+
+    for (x, y) in zip(lat, lon):
+        print(dist.find_closest(float(x), float(y)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
