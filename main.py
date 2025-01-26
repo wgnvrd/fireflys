4d337ea4f84170638500f6d6e632ed235f4a71de
RanivG <ranivgupta@gmail.com>
2025-01-25 23:01:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,28 +9,16 @@ MAP_KEY = os.getenv('FIRMS_MAP_KEY')
 
 url = 'https://firms.modaps.eosdis.nasa.gov/api/area/csv/' + str(MAP_KEY) + '/MODIS_NRT/-125,25,-64,50/10' #url and filter bounds for contigous US
 
-# def get_transaction_count() :
-#   count = 0
-#   try:
-#     df = pd.read_json(url,  typ='series')
-#     count = df['current_transactions']
-#   except:
-#     print ("Error in our call.")
-#   return count
-
-# print(get_transaction_count())
 
 dat = pd.read_csv(url) # data from url
 
 confLwrBnd = 50.0
 confUpperBnd = 100.0
 
-det = dat[dat["confidence"] >= confLwrBnd] # detected fires of concern
+det = dat.sort_values(by='confidence' , ascending=False)
+
+det = det[det["confidence"] >= confLwrBnd] # detected fires of concern
 det = det[det['confidence'] <= confUpperBnd] #filter out confirmed fires
-
-# print(det.describe()) # debug and verify confidence filtered correctly
-
-det = det.sort_values(by='confidence' , ascending=False)
 
 coords = det.loc[:,["latitude" , "longitude"]]
 
