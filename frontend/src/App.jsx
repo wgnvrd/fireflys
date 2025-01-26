f262ad9263a70c2b43c7886f3c58a641a7bcdca2
Isabelle Wagenvoord <isabellewagenvoord@gmail.com>
2025-01-26 01:05:26 -0600

    Add panels

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-// import "leaflet/dist/leaflet.css";
+import "leaflet/dist/leaflet.css";
+import 'bulma/css/bulma.min.css';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { MapContainer } from 'https://cdn.esm.sh/react-leaflet/MapContainer'
-import { TileLayer } from 'https://cdn.esm.sh/react-leaflet/TileLayer'
-import { useMap } from 'https://cdn.esm.sh/react-leaflet/hooks'
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { useMap } from 'react-leaflet/hooks'
 
 function App() {
-  const [state, setState] = useState(null);
+  // const [state, setState] = useState(null);
   // const [numbers, setNumbers] = useState("");
   // const [result, setResult] = useState(null);
 
@@ -26,20 +27,78 @@ function App() {
   //     console.error("Error analyzing data: ", error);
   //   }
   // };
-  return (
-    <div>
-      <h1>Leaflet Map</h1>
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        style={{ height: "100vh", backgroundColor: "grey" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
 
-      </MapContainer>
+  const hi = () => { console.log('hi'); }
+  useEffect(() => {
+    hi();
+    const intervalId = setInterval(hi, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div style={{height: "100vh"}}>
+      <nav className="navbar has-background-dark p-2 border">
+        <div className="navbar-brand">
+          <a className="navbar-item has-text-white has-text-weight-bold">
+            City Dashboard
+          </a>
+        </div>
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item has-text-white">Live View</a>
+            <a className="navbar-item has-text-white">Performance</a>
+          </div>
+          <div className="navbar-end">
+            <a className="navbar-item has-text-white">Export</a>
+            <a className="navbar-item has-text-white">Notifications</a>
+          </div>
+        </div>
+      </nav>
+      <div className="columns is-gapless" style={{height: "calc(100% - 52px)", width: "100vw"}}>
+        {/* Left Sidebar */}
+        <aside className="column is-2 has-background-dark p-3 border">
+          <p className="has-text-white has-text-weight-bold mb-2">NETWORK</p>
+            <ul>
+              <li className="has-text-white">Greenwood Village</li>
+              <ul>
+                <li className="has-text-grey-light">Holly</li>
+                <li className="has-text-grey-light">Orchard</li>
+                <li className="has-text-grey-light">Yosemite</li>
+                <li className="has-text-grey-light">Belleview</li>
+              </ul>
+            </ul>
+        </aside>
+        {/* Main map area */}
+        <div className="column is-7 border">
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+            style={{ height: "100%", width: "100%", backgroundColor: "grey" }}
+          >
+            <TileLayer
+              attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png"
+            />
+
+          </MapContainer>
+        </div>
+        {/* Right Sidebar */}
+        <aside className="column is-3 has-background-dark p-3 border">
+          <div>
+            <p className="has-text-weight-bold has-text-white">Intersection</p>
+            <div className="box">
+              <p><strong>Saturation:</strong> 62%</p>
+              <p><strong>Volume:</strong> 1340/hr</p>
+              <p><strong>Avg Delay:</strong> 73 sec</p>
+              <p><strong>Travel Time:</strong> 95 sec</p>
+            </div>
+            <div className="box">
+
+            </div>
+          </div>
+        </aside>
+      </div>
+      {/* <div id= */}
       {/* <h1>Data Analysis App</h1>
       <form onSubmit={handleSubmit}>
         <label>
